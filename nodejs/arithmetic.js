18ca66f6399e85bc4e846b1dc8490aab4dab8c0a
KishoreAnanth18 <kishoreananth.it20@bitsathy.ac.in>
2023-02-16 11:28:41 +0530

    nodejs

---
+++ nodejs/arithmetic.js
@@ -0,0 +1,40 @@
+//CLI input
+
+// To trim first 2 elements
+const arg = process.argv.slice(2);
+
+arg[1] = Number(arg[1]);
+arg[2] = Number(arg[2]);
+
+switch (arg[0]) {
+	case '+':
+		console.log(`Result of ${arg[1]} + ${arg[2]} = ${arg[1] + arg[2]}`);
+		break;
+        
+    //use /* in CLI
+	case '*':
+		console.log(`Result of ${arg[1]} * ${arg[2]} = ${arg[1] * arg[2]}`);
+		break;
+
+	case '-':
+		console.log(`Result of ${arg[1]} - ${arg[2]} = ${arg[1] - arg[2]}`);
+		break;
+
+	case '/':
+		if (arg[2] == 0) {
+			console.log('cannot be divided by zero!!');
+		} else {
+			console.log(`Result of ${arg[1]} / ${arg[2]} = ${arg[1] / arg[2]}`);
+		}
+		break;
+
+	case '%':
+		if (arg[2] == 0) {
+			console.log('cannot be divided by zero!!');
+		} else {
+			console.log(`Result of ${arg[1]} % ${arg[2]} = ${arg[1] % arg[2]}`);
+		}
+		break;
+
+	default: console.log(`operation cannot be performed!!`);
+}
