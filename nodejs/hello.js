7fbe90404bff1e28247a277e6b7beff283ce33cd
KishoreAnanth18 <kishoreananth.it20@bitsathy.ac.in>
2023-02-16 10:42:31 +0530

    commit for nodejs

---
+++ nodejs/hello.js
@@ -0,0 +1,20 @@
+// let age = '18'
+
+// if (age==18){
+//     console.log("Not eligible")
+// }else{
+//     console.log("Eligible")
+// }
+
+// data = [10,true,"kishroe",{name:"pradeepa", roll:139}]
+// for(let it=0; it<data.length; it++)
+//     console.log(data[it]);
+function returnSome(num1,num2){
+    console.log(num1+num2)
+}
+returnSome(10,20)
+
+let returnSomething = (num1,num2) => {
+    console.log(num1+num2);
+}
+returnSomething(10,20)
